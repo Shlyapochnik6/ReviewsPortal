b209433bccb6bffeb90705af3bee17ad2b836f09
Arseniy <dhhxdjsjdh@gmail.com>
2022-12-08 22:49:30 +0300

    add user registration command

---
+++ ReviewsPortal.Application/CommandsQueries/User/Commands/Registration/UserRegistrationCommand.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using ReviewsPortal.Application.Common.Mappings;
+
+namespace ReviewsPortal.Application.CommandsQueries.User.Commands.Registration;
+
+public class UserRegistrationCommand : IRequest, IMapWith<Domain.User>
+{
+    public string Email { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string Password { get; set; }
+    
+    public bool Remember { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<UserRegistrationCommand, Domain.User>()
+            .ForMember(u => u.UserName,
+                opt => opt.MapFrom(u => u.Name))
+            .ForMember(u => u.Email,
+                opt => opt.MapFrom(u => u.Email));
+    }
+}
---
+++ ReviewsPortal.Application/CommandsQueries/User/Commands/Registration/UserRegistrationCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ReviewsPortal.Application.Common.Exceptions;
+using ReviewsPortal.Application.Interfaces;
+
+namespace ReviewsPortal.Application.CommandsQueries.User.Commands.Registration;
+
+public class UserRegistrationCommandHandler : IRequestHandler<UserRegistrationCommand, Unit>
+{
+    private readonly IMapper _mapper;
+    private readonly IReviewsPortalDbContext _dbContext;
+    private readonly UserManager<Domain.User> _userManager;
+    private readonly SignInManager<Domain.User> _signInManager;
+
+    public UserRegistrationCommandHandler(IMapper mapper, IReviewsPortalDbContext dbContext, 
+        UserManager<Domain.User> userManager, SignInManager<Domain.User> signInManager)
+    {
+        _dbContext = dbContext;
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _mapper = mapper;
+    }
+    
+    public async Task<Unit> Handle(UserRegistrationCommand request, CancellationToken cancellationToken)
+    {
+        var existingUser = await GetExistingUser(request, cancellationToken);
+        if (existingUser) throw new ExistingUserException();
+        await RegisterUser(request);
+        return Unit.Value;
+    }
+
+    private async Task<bool> GetExistingUser(UserRegistrationCommand request, 
+        CancellationToken cancellationToken)
+    {
+        var existingUser = await _dbContext.Users.AnyAsync(u =>
+            u.Email == request.Email, cancellationToken);
+        return existingUser;
+    }
+
+    private async Task RegisterUser(UserRegistrationCommand request)
+    {
+        var user = _mapper.Map<Domain.User>(request);
+        await _userManager.CreateAsync(user, request.Password);
+        await _signInManager.SignInAsync(user, request.Remember);
+    }
+}
