f647aff25f1410ca39f372fee0be3744a10faefb
Arseniy <dhhxdjsjdh@gmail.com>
2022-12-08 19:41:59 +0300

    add validation to registration page

--- ReviewsPortal.Web/ClientApp/src/app/app.module.ts
+++ ReviewsPortal.Web/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {RouterModule} from '@angular/router';
 
@@ -21,15 +21,16 @@ import {FetchDataComponent} from './fetch-data/fetch-data.component';
     FetchDataComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      {path: '', component: HomeComponent, pathMatch: 'full'},
+      {path: '', component: RegistrationComponent, pathMatch: 'full'},
       {path: 'counter', component: CounterComponent},
       {path: 'fetch-data', component: FetchDataComponent},
       {path: 'registration', component: RegistrationComponent}
-    ])
+    ]),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ReviewsPortal.Web/ClientApp/src/app/registration/registration.component.ts
+++ ReviewsPortal.Web/ClientApp/src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 ﻿import {Component} from "@angular/core";
+import {AbstractControl, FormGroup, FormControl, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-registration',
@@ -7,5 +8,33 @@
 })
 
 export class RegistrationComponent{
+  checkPassword: ValidatorFn = (group: AbstractControl): ValidationErrors | null => {
+    const confirm = group.get('confirm')?.value;
+    const password = group.get('password')?.value;
+    group.get('rememberMe')?.clearValidators();
+    return password === confirm ? null : {notSame: true};
+  }
 
+  registrationForm = new FormGroup({
+    'email': new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    'userName': new FormControl('', [
+      Validators.required,
+      Validators.pattern("^[a-zA-Z0-9_]*$")
+    ]),
+    'password': new FormControl('', [
+      Validators.required,
+      Validators.minLength(5)
+    ]),
+    'confirm': new FormControl('', [
+      Validators.required
+    ]),
+    'rememberMe': new FormControl(false)},
+    {validators: this.checkPassword})
+
+  onSubmit() {
+    console.log(this.registrationForm?.controls.rememberMe);
+  }
 }
