4304e3ad2d8548e8c41700f1ba856bb219ff17cf
Arseniy <dhhxdjsjdh@gmail.com>
2022-12-09 22:52:47 +0300

    add form submission login

--- ReviewsPortal.Web/ClientApp/src/app/login/login.component.ts
+++ ReviewsPortal.Web/ClientApp/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 ﻿import {Component} from "@angular/core";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -8,6 +10,10 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 })
 
 export class LoginComponent {
+  error?: string;
+
+  constructor(private http: HttpClient, private router: Router) {
+  }
 
   loginForm: FormGroup = new FormGroup({
     'email': new FormControl('', [
@@ -22,5 +28,15 @@ export class LoginComponent {
 
   onSubmit(){
     console.log(this.loginForm.value)
+    this.http.post('api/user/login', this.loginForm.value)
+      .subscribe({
+        next: _ => this.router.navigate(['/']),
+        error: err => {
+          if (err.status === 401)
+            this.error = 'The entered password is not consistent with the user password!'
+          if (err.status === 404)
+            this.error = 'The entered user was not found!'
+        }
+      })
   }
 }
