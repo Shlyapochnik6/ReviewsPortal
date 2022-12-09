42bab70b5dc948137d24fdd24a9a9eb51fe45d42
Arseniy <dhhxdjsjdh@gmail.com>
2022-12-09 22:14:48 +0300

    add user login query

---
+++ ReviewsPortal.Application/CommandsQueries/User/Queries/Login/UserLoginQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace ReviewsPortal.Application.CommandsQueries.User.Queries.Login;
+
+public class UserLoginQuery : IRequest
+{
+    public string Email { get; set; }
+    
+    public string Password { get; set; }
+
+    public bool Remember { get; set; }
+}
---
+++ ReviewsPortal.Application/Common/Exceptions/UnfoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace ReviewsPortal.Application.Common.Exceptions;
+
+public class UnfoundException : Exception
+{
+    public UnfoundException(string name) :
+        base($"{name} wasn't found") { }
+}
