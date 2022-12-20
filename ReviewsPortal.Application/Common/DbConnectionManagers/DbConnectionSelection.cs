de68a5453eb7cdbbe93f4c471650ee4c11969f22
Arseniy <dhhxdjsjdh@gmail.com>
2022-12-20 18:52:54 +0300

    change cloud database

--- ReviewsPortal.Application/Common/DbConnectionManagers/DbConnectionSelection.cs
+++ ReviewsPortal.Application/Common/DbConnectionManagers/DbConnectionSelection.cs
@@ -8,7 +8,7 @@ public class DbConnectionSelection : IDbConnectionSelection
     {
         var e = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
         var result = e == "Production" ? 
-            "Host=dpg-ce751q14rebdt3d6cbf0-a;Port=5432;Database=reviews_db;Username=reviews_db_user;Password=n67kVQ1jBwP1c208G3DbmxgFg2QIWLE7" :
+            "Host=dpg-cegthr82i3mkhvq8277g-a;Port=5432;Database=reviews_db_7bay;Username=kinelak;Password=K2e8pekfVBkBxEgjJX7lmuPZdrzPfRIf" :
             "Host=localhost;Port=5432;Database=ReviewsPortal;Username=postgres;Password=sa";
         return result;
     }
