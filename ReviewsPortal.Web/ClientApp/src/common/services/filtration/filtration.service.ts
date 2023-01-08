bb58f9b59e800f36dfe520a0c59c47aadc8ee09e
Arseniy <dhhxdjsjdh@gmail.com>
2023-01-08 11:40:09 +0300

    fix filter by date

---
+++ ReviewsPortal.Web/ClientApp/src/common/services/filtration/filtration.service.ts
@@ -0,0 +1,20 @@
+﻿import {Injectable} from "@angular/core";
+import {filterBy, FilterOperator} from "@progress/kendo-data-query";
+import {BriefReviewModel} from "../../models/BriefReviewModel";
+import {AllUserReviewsModel} from "../../models/AllUserReviewsModel";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FiltrationService {
+  filterData(fieldName: string, filterText: string,
+             data: AllUserReviewsModel[]): AllUserReviewsModel[] {
+    return filterBy(data, {
+      logic: 'and',
+      filters: [
+        {field: fieldName, value: filterText, operator: 'contains', ignoreCase: true}
+      ]
+    })
+  }
+}
